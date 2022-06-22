4469956aadf21f8863542e42dca3b6747ec94805
Yuvaraj-Ayyasamy <105203963+Yuvaraj-Ayyasamy@users.noreply.github.com>
2022-06-22 14:31:47 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -11,19 +11,22 @@ int networkAlertStub(float celcius) {
     // Return 200 for ok
     // Return 500 for not-ok
     // stub always succeeds and returns 200
-    return 200;
+    if(celcius<200) {
+        return 200;
+    } else {
+        return 500;
+    }
 }
 
 #if(ENABLE_PRODUCTION_CODE == PRODUCTIONCODE_OFF)
 void testalertInCelcius(float celcius, int returnCode) {
     assert(celcius<returnCode);
-}  
+}
 #endif
 
 void alertInCelcius(float farenheit) {
     float celcius = (farenheit - 32) * 5 / 9;
     int returnCode = networkAlertStub(celcius);
-#if(ENABLE_PRODUCTION_CODE == PRODUCTIONCODE_ON)
     if (returnCode != 200) {
         // non-ok response is not an error! Issues happen in life!
         // let us keep a count of failures to report
@@ -31,14 +34,24 @@ void alertInCelcius(float farenheit) {
         // Add a test below to catch this bug. Alter the stub above, if needed.
         alertFailureCount += 0;
     }
-#else
-    testalertInCelcius(celcius, returnCode);
-#endif
 }
 
 int main() {
     alertInCelcius(400.5);
+#if(ENABLE_PRODUCTION_CODE == PRODUCTIONCODE_OFF)
+    assert(alertFailureCount == 1);
+    testalertInCelcius(celcius, returnCode);
+#endif
+    alertInCelcius(392.0);
+#if(ENABLE_PRODUCTION_CODE == PRODUCTIONCODE_OFF)
+    assert(alertFailureCount == 1);
+    testalertInCelcius(celcius, returnCode);
+#endif
     alertInCelcius(303.6);
+#if(ENABLE_PRODUCTION_CODE == PRODUCTIONCODE_OFF)
+    assert(alertFailureCount == 1);
+    testalertInCelcius(celcius, returnCode);
+#endif
     printf("%d alerts failed.\n", alertFailureCount);
     printf("All is well (maybe!)\n");
     return 0;
