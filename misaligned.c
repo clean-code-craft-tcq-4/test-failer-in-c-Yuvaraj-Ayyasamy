2e80800887e32c2287068bb6341748cc3164d9b2
Yuvaraj-Ayyasamy <105203963+Yuvaraj-Ayyasamy@users.noreply.github.com>
2022-06-22 14:44:57 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -5,7 +5,6 @@
 #define COLORMANUAL_TEXTSIZE 20*25
 char colorMap[COLORMANUAL_TEXTSIZE];
 char referenceColorMap[COLORMANUAL_TEXTSIZE];
-
 const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
 const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
 int numberOfMajorColors = sizeof(majorColor) / sizeof(majorColor[0]);
@@ -14,7 +13,6 @@ int numberOfMinorColors = sizeof(minorColor) / sizeof(minorColor[0]);
 int printColorMap() 
 {
     int majorIndex, minorIndex, pairNumber = 0, pairLength = 0;
-
     for(majorIndex = 0; majorIndex < numberOfMajorColors; majorIndex++) {
         for(minorIndex = 0; minorIndex < numberOfMinorColors; minorIndex++) {
             pairLength = sprintf(&colorMap[pairNumber], "%d |%s |%s\n", (majorIndex * numberOfMajorColors + minorIndex)+1, majorColor[majorIndex], minorColor[minorIndex]);
@@ -23,11 +21,9 @@ int printColorMap()
     }
     return majorIndex * minorIndex;
 }
-
 void colorCodeReferenceManual()
 {
     int majorIndex, minorIndex, pairNumber = 0, pairLength = 0;
-    
     for(majorIndex = 0; majorIndex < numberOfMajorColors; majorIndex++) {
         for(minorIndex = 1; minorIndex <= numberOfMinorColors; minorIndex++) {
             pairLength = sprintf(&referenceColorMap[pairNumber], "%d\t|%s\t|%s\n", majorIndex * numberOfMajorColors + minorIndex, majorColor[majorIndex], minorColor[minorIndex-1]);
@@ -35,7 +31,6 @@ void colorCodeReferenceManual()
         }
     }
 }
-
 void testManualAlignment()
 {
     int result;
@@ -43,7 +38,6 @@ void testManualAlignment()
     result = strcmp(&referenceColorMap[0], &colorMap[0]);
     assert( result == 0);     
 }
-
 int main() 
 {
     int result = printColorMap();
