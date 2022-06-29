8e125bed0b627f4528062805c2a9702ef28a1175
Yuvaraj-Ayyasamy <105203963+Yuvaraj-Ayyasamy@users.noreply.github.com>
2022-06-29 16:14:29 +0530

    Update alerter.h

--- alerter.h
+++ alerter.h
@@ -2,8 +2,11 @@
 #include <assert.h>
 
 #define THRESHOLD_CELCIUS_VALUE 200
+#define OK    200
+#define NOTOK 500
 
 int alertFailureCount = 0;
 
 int networkAlertStub(float celcius);
 void alertInCelcius(float farenheit);
+float farenheitToCelcius(float farenheit);
